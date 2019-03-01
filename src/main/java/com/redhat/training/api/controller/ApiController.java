f6a72bed27a72e526c54d8946c5ea2d20608a181
Douglas Silva <dsilva@redhat.com>
2019-02-28 23:05:06 -0300

    update

--- src/main/java/com/redhat/training/api/controller/ApiController.java
+++ src/main/java/com/redhat/training/api/controller/ApiController.java
@@ -16,6 +16,6 @@ public class ApiController {
 
     @GetMapping("/protected/hello/{name}")
     protected String protectedHello(@PathVariable String name){
-        return "Hello, "+name;
+        return "Hello, "+name+". This is a secured endpoint!";
     }
 }
